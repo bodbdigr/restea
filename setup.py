f619fb92a0ff43b4a03858d06cb7637a02efe749
Walery Jadłowski <valery@rebelmouse.com>
2015-03-26 17:29:47 +0100

    fix setup.py repo urls

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     description='Simple RESTful server toolkit',
     author='Walery Jadłowski',
     author_email='bodb.digr@gmail.com',
-    url='https://github.com/bodbdigr/rest.py',
-    download_url='https://github.com/bodbdigr/rest.py/archive/v0.1.0.tar.gz',
+    url='https://github.com/bodbdigr/restea',
+    download_url='https://github.com/bodbdigr/restea/archive/v0.1.0.tar.gz',
     keywords=['rest', 'restful', 'restea'],
     classifiers=[]
 )
