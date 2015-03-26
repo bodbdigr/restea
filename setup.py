9dc74b4aa7e584e9711f0ebeb04c2d1da3c1329c
Walery Jadłowski <valery@rebelmouse.com>
2015-03-26 17:32:36 +0100

    updated version. v0.1.1

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name='restea',
     packages=['restea'],
-    version='0.1.0',
+    version='0.1.1',
     description='Simple RESTful server toolkit',
     author='Walery Jadłowski',
     author_email='bodb.digr@gmail.com',
     url='https://github.com/bodbdigr/restea',
-    download_url='https://github.com/bodbdigr/restea/archive/v0.1.0.tar.gz',
+    download_url='https://github.com/bodbdigr/restea/archive/v0.1.1.tar.gz',
     keywords=['rest', 'restful', 'restea'],
     classifiers=[]
 )
