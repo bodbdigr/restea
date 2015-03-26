cc6d440d147b6292bb8dbb8a22b34f33b64f151f
Walery Jadłowski <valery@rebelmouse.com>
2015-03-26 16:32:16 +0100

    added package setup code and license

--- setup.py
+++ setup.py
@@ -1,2 +1,15 @@
-def setup():
-    pass
+from distutils.core import setup
+
+
+setup(
+    name='rest.py',
+    packages=['rest.py'],
+    version='0.1.0',
+    description='Simple RESTful server toolkit',
+    author='Walery Jadłowski',
+    author_email='bodb.digr@gmail.com',
+    url='https://github.com/bodbdigr/rest.py',
+    download_url='https://github.com/bodbdigr/rest.py/archive/v0.1.0.tar.gz',
+    keywords=['rest', 'restful', 'rest.py'],
+    classifiers=[]
+)
