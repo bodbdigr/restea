7d6e7994cf77e5e159ff3a8d42b3c9b19f5b12da
Jorge Puente Sarrín <jorge@puentesarr.in>
2015-12-15 13:27:18 -0500

    Bump 0.2.2 version.

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name='restea',
     packages=['restea', 'restea.adapters'],
-    version='0.2.1',
+    version='0.2.2',
     description='Simple RESTful server toolkit',
     author='Walery Jadlowski',
     author_email='bodb.digr@gmail.com',
     url='https://github.com/bodbdigr/restea',
-    download_url='https://github.com/bodbdigr/restea/archive/0.2.1.tar.gz',
+    download_url='https://github.com/bodbdigr/restea/archive/0.2.2.tar.gz',
     keywords=['rest', 'restful', 'restea'],
     license='MIT',
     classifiers=[
