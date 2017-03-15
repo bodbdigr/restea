34ef2ee683a259a05e530514e743a9f69fa5e663
Krzysztof Klinikowski <kkszysiu@gmail.com>
2017-03-16 01:40:44 +0800

    Bump version of pytest

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'pytest-runner',
     ],
     tests_require=[
-        'pytest==3.0.6',
+        'pytest==3.0.7',
         'pytest-cov==2.4.0',
         'pytest-mock==1.5.0',
     ],
